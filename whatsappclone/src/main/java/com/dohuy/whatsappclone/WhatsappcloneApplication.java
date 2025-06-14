476855ab136f8423d2e1d651a8ebb57af48015bd
dovanhuy2003za <dovanhuy2003za@gmail.com>
2025-06-14 15:39:00 +0700

    feat:OpenApi

--- whatsappclone/src/main/java/com/dohuy/whatsappclone/WhatsappcloneApplication.java
+++ whatsappclone/src/main/java/com/dohuy/whatsappclone/WhatsappcloneApplication.java
@@ -2,8 +2,17 @@ package com.dohuy.whatsappclone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.OAuthFlow;
+import io.swagger.v3.oas.annotations.security.OAuthFlows;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 
 @SpringBootApplication
+@EnableJpaAuditing
+@SecurityScheme(name = "keycloak", type = SecuritySchemeType.OAUTH2, bearerFormat = "JWT", scheme = "bearer", in = SecuritySchemeIn.HEADER, flows = @OAuthFlows(password = @OAuthFlow(authorizationUrl = "http://localhost:9090/realms/whatsapp-clone/protocol/openid-connect/auth", tokenUrl = "http://localhost:9090/realms/whatsapp-clone/protocol/openid-connect/token")))
 public class WhatsappcloneApplication {
 
 	public static void main(String[] args) {
---
+++ whatsappclone/src/main/java/com/dohuy/whatsappclone/notification/NotificationType.java
@@ -0,0 +1,9 @@
+package com.dohuy.whatsappclone.notification;
+
+public enum NotificationType {
+    SEEN,
+    MESSAGE,
+    IMAGE,
+    AUDIO,
+    VIDEO,
+}
