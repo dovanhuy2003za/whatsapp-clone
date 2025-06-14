6fa8f9a4dfa7bb70325bd72114286ef16b98e4de
dovanhuy2003za <dovanhuy2003za@gmail.com>
2025-06-14 17:24:25 +0700

    feat:config keycloak ui

--- whatsapp-clone-ui/src/app/app.config.ts
+++ whatsapp-clone-ui/src/app/app.config.ts
@@ -1,12 +1,24 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, inject, provideAppInitializer, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { KeycloakService } from './utils/keycloak/keycloak.service';
+import { keycloakHttpInterceptor } from './utils/http/keycloak-http-interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient(
+      withInterceptors([keycloakHttpInterceptor])
+    ),
+    provideAppInitializer(() => {
+      const initFn = ((key: KeycloakService) => {
+        return () => key.init()
+      })(inject(KeycloakService));
+      return initFn();
+    })
   ]
 };
